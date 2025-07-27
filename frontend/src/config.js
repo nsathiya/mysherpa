26600eeede535171e9e721560f43e5944a0adb82
Naren Sathiya <narensathiya@Mac.attlocal.net>
2025-07-26 19:23:26 -0500

    deploy: back to nixpacks

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,8 +1,6 @@
 // Configuration file for environment variables
 const config = {
   backendUrl: process.env.REACT_APP_BACKEND_URL || 'http://localhost:3000',
-  
-  // Google Maps configuration
   googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY || '',
   googleMapId: process.env.REACT_APP_GOOGLE_MAP_ID || '',
 };
